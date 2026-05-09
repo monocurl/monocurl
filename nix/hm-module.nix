989c8cfce1a27976dfe3d8645abb1d479cce427c
Vitalii Lukyanov <tukanoid@outlook.com>
2026-05-09 15:04:41 +0200

    mixed up apis

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -30,7 +30,7 @@ in
             }: {
               name = ".local/share/icons/hicolor/${size}/apps/monocurl.png";
               value = {
-                src = ../assets/AppIcon.appiconset + "/${filename}";
+                source = ../assets/AppIcon.appiconset + "/${filename}";
               };
             })
             iconSet.images);
